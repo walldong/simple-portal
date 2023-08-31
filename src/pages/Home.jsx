5b18ee6c79b422d6d799de7d3794ec7314b33a36
欧阳会东(10032865) <ouyanghd@glodon.com>
2023-08-31 16:02:58 +0800

    update default pages-Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,37 @@
 import { React, Component } from "react";
+import { Carousel } from "antd";
+
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 export default class Home extends Component {
     constructor(props) {
         super(props)
-        this.state={
-            
+        this.state = {
+
         }
 
     }
     render() {
         return <div>
+            <Carousel autoplay effect="fade">
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
             Home
         </div>
     }
