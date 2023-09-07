e55efceb965d2d64253b493b0343c21a837584ac
欧阳会东(10032865) <ouyanghd@glodon.com>
2023-09-07 18:54:49 +0800

    feat(update home page): add carousel card and paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { React, Component } from "react";
-import { Carousel } from "antd";
-
+import { Carousel, Card, Col, Row, Typography } from "antd";
+const { Paragraph } = Typography;
 const contentStyle = {
-    height: '160px',
+    height: '240px',
     color: '#fff',
     lineHeight: '160px',
     textAlign: 'center',
     background: '#364d79',
 };
+const homeBoxStyle = {
+    height: '800px',
+    marginTop: "24px"
+}
 export default class Home extends Component {
     constructor(props) {
         super(props)
@@ -16,6 +20,7 @@ export default class Home extends Component {
         }
 
     }
+
     render() {
         return <div>
             <Carousel autoplay effect="fade">
@@ -32,7 +37,38 @@ export default class Home extends Component {
                     <h3 style={contentStyle}>4</h3>
                 </div>
             </Carousel>
-            Home
+            <div style={homeBoxStyle}>
+                <Row gutter={24} justify="space-evenly">
+                    <Col span={4}>
+                        <Card title="Card title" bordered={false} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
+                            Card content
+                        </Card>
+                    </Col>
+                    <Col span={4}>
+                        <Card title="Card title" bordered={false} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
+                            Card content
+                        </Card>
+                    </Col>
+                    <Col span={4}>
+                        <Card title="Card title" bordered={false} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
+                            Card content
+                        </Card>
+                    </Col>
+                    <Col span={4}>
+                        <Card title="Card title" bordered={false} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
+                            Card content
+                        </Card>
+                    </Col>
+                </Row>
+                <Paragraph ellipsis={false} marginXS={24}>
+                    Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
+                    Design, a design language for background applications, is refined by Ant UED Team. Ant
+                    Design, a design language for background applications, is refined by Ant UED Team. Ant
+                    Design, a design language for background applications, is refined by Ant UED Team. Ant
+                    Design, a design language for background applications, is refined by Ant UED Team. Ant
+                    Design, a design language for background applications, is refined by Ant UED Team.
+                </Paragraph>
+            </div>
         </div>
     }
 }
